10b61b918a731f6ed5c1cc4786d29c862b345d14
LordSosukeAizen <gudipraneeth175@gmail.com>
2025-06-14 22:15:08 +0530

    ETL of streaming data is completed

--- dags/dag.py
+++ dags/dag.py
@@ -1,88 +1,20 @@
-import logging
 import json
-import functools
-from airflow import DAG
-from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
-from airflow.providers.apache.kafka.sensors.kafka import AwaitMessageSensor
-from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.apache.kafka.hooks.consume import KafkaConsumerHook
+from airflow.sdk import task, dag
+from airflow.exceptions import AirflowException
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-consumer_logger = logging.getLogger("airflow")
 
-KAFKA_TOPIC = "stock-data"
+KAFKA_TOPIC = ["stock-data"]
 KAFKA_CONFIG_ID = "TEST"
 POSTGRES_CONN_ID = "postgres"
 
-# Modified consumer function to handle both message and context
-def consumer_function(message, ti):
-    """
-    This function consumes data from the producer.
-    """
-    decoded_msg = message.value().decode("utf-8")
-    consumer_logger.info("Kafka Msg: %s - %s", message.key(), decoded_msg)
-    ti.xcom_push(key="key", value=decoded_msg)
+@dag(
+ schedule="@hourly"   
+)
+def consume_kafka_msgs():
     
-
-# Clean and transform
-def clean_and_transform(**context):
-    """
-    Transforms decoded JSON string to tuple.
-    """
-    messages = context['ti'].xcom_pull(task_ids="consume_from_topic", key="key")
-    print("Decoded Message ---", decoded_msg)
-    
-    if not messages:
-        raise ValueError("No messages found from Kafka.")
-
-    decoded_msg = messages[0]  
-
-    try:
-        msg = json.loads(decoded_msg)
-
-        transformed = (
-            msg["open"],
-            msg["close"],
-            msg["low"],
-            msg["high"],
-            msg["volume"],
-            msg["vwap"],
-            msg["timestamp"],
-            msg["transactions"],
-        )
-        
-        return transformed
-
-    except Exception as e:
-        raise ValueError(f"Invalid message: {decoded_msg} — {e}")
-
-
-# store to postgres
-def store_to_postgres(**context):
-    """
-    Stores cleaned data into PostgreSQL.
-    """
-    transformed_data = context['ti'].xcom_pull(task_ids="clean_and_transform")
-    if not transformed_data:
-        print("No data to store.")
-        return
-
-    query = """
-    INSERT INTO stocks_data (open, close, low, high, volume, vwap, timestamp, transactions)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-    """
-
-    hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-    conn = hook.get_conn()
-    cursor = conn.cursor()
-    cursor.execute(query, transformed_data)
-    conn.commit()
-    print("Inserted to PostgreSQL.")
-
-with DAG("consume_kafka_msgs",
-         schedule="@hourly") as dag:
-    
-    # Create table if not exist
     query = """
     CREATE TABLE IF NOT EXISTS stocks_data (
         id SERIAL PRIMARY KEY,
@@ -102,26 +34,114 @@ with DAG("consume_kafka_msgs",
         sql=query
     )
     
-    consumer = ConsumeFromTopicOperator(
-        kafka_config_id=KAFKA_CONFIG_ID,
-        task_id="consume_from_topic",
-        topics=[KAFKA_TOPIC],
-        apply_function=consumer_function,
-        apply_function_kwargs={'ti' : '{{ti}}' },
-        max_messages=1,
-        max_batch_size=1
-    )
-
-    clean = PythonOperator(
-        task_id="clean_and_transform",
-        python_callable=clean_and_transform,    
-  
-    )
     
-    to_postgres = PythonOperator(
-        task_id="store_to_postgres",
-        python_callable=store_to_postgres,
+    # Consumer Using Consumer Hook
+    @task
+    def consumer_from_topic(BATCH_SIZE=10, MAX_MESSAGES=100):
+        """
+        This Task Consumes from Kafka Topic
+        """
+        consumer = KafkaConsumerHook(
+            topics=KAFKA_TOPIC,
+            kafka_config_id=KAFKA_CONFIG_ID
+        ).get_consumer()
 
-    )
+        if not consumer:
+            raise AirflowException("Kafka connection failed. Check config and topic.")
+
+        try:
+            msg_collector = []
+            remaining = MAX_MESSAGES
+
+            while remaining > 0:
+                msgs = consumer.consume(num_messages=BATCH_SIZE)
+                print("Messages -- ", msgs)
+                for msg in msgs:
+                    val = msg.value().decode("utf-8")
+                    print(f"Received: {msg.key()} --- {val}")
+                    msg_collector.append(val)
+                remaining -= min(len(msgs), remaining)
+
+            consumer.close()
+            print("Consumer closed.")
+
+            return msg_collector 
+
+        except Exception as e:
+            raise AirflowException(f"Kafka consumption error: {e}")
+
+            
+    @task
+    def clean_and_transform(messages):
+            """
+            Transforms decoded JSON string to tuple.
+            """
+
+            transformed_messages = []
+            for message in messages:
+                print("Decoded Message ---", message)
+                if not message:
+                    raise ValueError("No messages found from Kafka.")
+
+                decoded_msg = message
+                try:
+                    msg = json.loads(decoded_msg)
+
+                    transformed = (
+                        msg["open"],
+                        msg["close"],
+                        msg["low"],
+                        msg["high"],
+                        msg["volume"],
+                        msg["vwap"],
+                        msg["timestamp"],
+                        msg["transactions"],
+                    )
+                    
+                    transformed_messages.append(transformed)
+
+            
+                except Exception as e:
+                    raise ValueError(f"Invalid message: {decoded_msg} — {e}")
+
+            return transformed_messages
+            
+            
+    @task
+    def store_to_postgres(transformed_data):
+        """
+        Stores cleaned data into PostgreSQL.
+        """
+        if not transformed_data:
+            print("No data to store.")
+            return
+
+        query = """
+        INSERT INTO stocks_data (open, close, low, high, volume, vwap, timestamp, transactions)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        conn = hook.get_conn()
+        cursor = conn.cursor()
+        try:
+            cursor.executemany(query, transformed_data)
+            conn.commit()
+            print(f"Inserted {len(transformed_data)} rows into PostgreSQL.")
+        except Exception as e:
+            conn.rollback()
+            print("Failed to insert data:", e)
+            raise
+        finally:
+            cursor.close()
+            conn.close()
+
+        
+            
+    consume = consumer_from_topic()
+    clean = clean_and_transform(consume)
+    load_data_to_db = store_to_postgres(clean)
+    create_table >> consume >> clean >> load_data_to_db
     
-    create_table >> consumer >> clean >> to_postgres
+    
+dag = consume_kafka_msgs()
